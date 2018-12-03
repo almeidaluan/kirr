52a9069fd57ebeec82ae4b9e58812bf84215c189
luan <almeida.luan099@gmail.com>
2018-12-02 23:38:42 -0200

    Criaçao do Model e Registrando no Admin

--- shortener/models.py
+++ shortener/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Url(models.Model):
+    url = models.CharField(max_length=255)
+    shorturl = models.CharField(max_length=15,unique=True)
+
+    def __str__(self):
+        return self.url
