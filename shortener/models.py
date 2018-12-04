dd03ce398370e5963b9135dd8cf3379275fdf578
luan <almeida.luan099@gmail.com>
2018-12-04 10:47:13 -0200

    Campos Updated,Created + sobrescrita do metodo save

--- shortener/models.py
+++ shortener/models.py
@@ -1,8 +1,18 @@
 from django.db import models
 
 class Url(models.Model):
+
     url = models.CharField(max_length=255)
     shorturl = models.CharField(max_length=15,unique=True)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+
+    def save(self, *args, **kwargs):
+        super(Url,self).save(*args, **kwargs)
+
+    def my_self(self):
+        super(Url,self).save(*args,**kwargs)
 
     def __str__(self):
         return self.url
